d688c1ddf125b0ca792a5f6db03c33bd84afc239
Julien Terrier <julien.terrier@orange.com>
2020-01-23 13:45:44 +0100

    Gère le déplacement de plusieurs carts (sans collision)

--- src/main/kotlin/Day13.kt
+++ src/main/kotlin/Day13.kt
@@ -1,4 +1,48 @@
-enum class Cart(val char: Char) {
+
+import CartOrientation.FACING_BOTTOM
+import CartOrientation.FACING_LEFT
+import CartOrientation.FACING_RIGHT
+import CartOrientation.FACING_TOP
+import Path.BOTTOM_TO_LEFT
+import Path.BOTTOM_TO_RIGHT
+import Path.HORIZONTAL
+import Path.INTERSECTION
+import Path.VERTICAL
+
+data class Cart(val orientation: CartOrientation, val point: Point) {
+
+    fun move(paths: Map<Point, Path>): Cart {
+
+        val nextPoint = when (orientation) {
+            FACING_LEFT -> Point(point.x - 1, point.y)
+            FACING_RIGHT -> Point(point.x + 1, point.y)
+            FACING_TOP -> Point(point.x, point.y - 1)
+            FACING_BOTTOM -> Point(point.x, point.y + 1)
+        }
+
+        val nextOrientation = when (paths.getValue(nextPoint)) {
+            VERTICAL -> orientation
+            HORIZONTAL -> orientation
+            BOTTOM_TO_RIGHT -> when (orientation) {
+                FACING_LEFT -> FACING_BOTTOM
+                FACING_RIGHT -> FACING_TOP
+                FACING_TOP -> FACING_RIGHT
+                FACING_BOTTOM -> FACING_LEFT
+            }
+            BOTTOM_TO_LEFT -> when (orientation) {
+                FACING_LEFT -> FACING_TOP
+                FACING_RIGHT -> FACING_BOTTOM
+                FACING_TOP -> FACING_LEFT
+                FACING_BOTTOM -> FACING_RIGHT
+            }
+            INTERSECTION -> orientation
+        }
+
+        return Cart(nextOrientation, nextPoint)
+    }
+}
+
+enum class CartOrientation(val char: Char) {
     FACING_LEFT('<'),
     FACING_RIGHT('>'),
     FACING_TOP('^'),
@@ -7,12 +51,12 @@ enum class Cart(val char: Char) {
 
     val path: Path
         get() = when (this) {
-            FACING_LEFT, FACING_RIGHT -> Path.HORIZONTAL
-            FACING_TOP, FACING_BOTTOM -> Path.VERTICAL
+            FACING_LEFT, FACING_RIGHT -> HORIZONTAL
+            FACING_TOP, FACING_BOTTOM -> VERTICAL
         }
 
     companion object {
-        fun parse(char: Char): Cart? = values().firstOrNull { it.char == char }
+        fun parse(char: Char): CartOrientation? = values().firstOrNull { it.char == char }
     }
 }
 
@@ -25,8 +69,8 @@ enum class Path(val char: Char) {
     ;
 
     companion object {
-        fun parse(char: Char, cart: Cart?): Path? {
-            cart?.let {
+        fun parse(char: Char, cartOrientation: CartOrientation?): Path? {
+            cartOrientation?.let {
                 return it.path
             }
 
@@ -35,23 +79,38 @@ enum class Path(val char: Char) {
 }
 
 data class Tracks(
-    val paths: Map<Point, Path?>,
-    val carts: Map<Point, Cart?> // TODO List<Cart> pour que chaque Cart ait un état (virages)
+    val paths: Map<Point, Path>,
+    val carts: List<Cart>
 ) {
+    fun move(): Tracks {
+
+        val movedCarts = carts.map { it.move(paths) } // TODO order
+
+        return this.copy(carts = movedCarts)
+    }
+
     companion object {
         fun from(input: String): Tracks {
             val (carts, paths) = input
                 .split("\n")
                 .mapIndexed { y, line ->
                     line.toCharArray().mapIndexed { x, char ->
-                        val cart = Cart.parse(char)
-                        val path = Path.parse(char, cart)
-                        (Point(x, y) to cart) to (Point(x, y) to path)
+                        val orientation = CartOrientation.parse(char)
+
+                        val cart = if (orientation != null) Cart(orientation, Point(x, y)) else null
+                        val path = Path.parse(char, orientation)
+
+                        val pointPathPair = if (path != null) (Point(x, y) to path) else null
+
+                        cart to pointPathPair
                     }
                 }.flatten()
                 .unzip()
 
-            return Tracks(carts = carts.toMap(), paths = paths.toMap())
+            return Tracks(
+                carts = carts.filterNotNull(),
+                paths = paths.filterNotNull().toMap()
+            )
         }
     }
 }
--- src/test/kotlin/Day13Test.kt
+++ src/test/kotlin/Day13Test.kt
@@ -1,3 +1,7 @@
+import CartOrientation.FACING_BOTTOM
+import CartOrientation.FACING_LEFT
+import CartOrientation.FACING_RIGHT
+import io.kotlintest.matchers.collections.shouldHaveSize
 import io.kotlintest.shouldBe
 import org.junit.jupiter.api.Test
 
@@ -20,7 +24,47 @@ class Day13Test {
         paths[5, 3] shouldBe Path.BOTTOM_TO_RIGHT
         paths[0, 2] shouldBe Path.VERTICAL
 
-        carts[0, 2] shouldBe Cart.FACING_BOTTOM
+        carts shouldHaveSize 1
 
+        carts[0] shouldBe Cart(orientation = FACING_BOTTOM, point = Point(0, 2))
+    }
+
+    @Test
+    fun `should move a single cart`() {
+        val initialTracks = Tracks.from(
+            """
+     /----\
+     |    |
+     v    |
+     \----/
+     """.trimIndent()
+        )
+
+        val sequence = generateSequence(initialTracks) {
+            it.move()
+        }
+
+        val (pathsAfter1Move, cartsAfter1Move) = sequence.move(1)
+
+        checkPathsHaveNotChanged(pathsAfter1Move)
+        cartsAfter1Move shouldHaveSize 1
+        cartsAfter1Move[0] shouldBe Cart(orientation = FACING_RIGHT, point = Point(0, 3))
+
+
+        val (pathsAfter10Moves, cartsAfter10Moves) = sequence.move(10)
+
+        checkPathsHaveNotChanged(pathsAfter1Move)
+        cartsAfter10Moves shouldHaveSize 1
+        cartsAfter10Moves[0] shouldBe Cart(orientation = FACING_LEFT, point = Point(4, 0))
+    }
+
+    private fun Sequence<Tracks>.move(times: Int) = take(times + 1).last()
+
+    private fun checkPathsHaveNotChanged(paths: Map<Point, Path>) {
+        paths[0, 0] shouldBe Path.BOTTOM_TO_RIGHT
+        paths[1, 0] shouldBe Path.HORIZONTAL
+        paths[0, 1] shouldBe Path.VERTICAL
+        paths[5, 3] shouldBe Path.BOTTOM_TO_RIGHT
+        paths[0, 2] shouldBe Path.VERTICAL
     }
 }
